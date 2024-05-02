ca43cf959fbc826cd8504423853edb24e616c359
Karimtabikh <karim.tabikh@outlook.com>
2024-05-02 18:46:43 +0300

    Basic prisma

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import type { OnModuleDestroy, OnModuleInit } from "@nestjs/common";
+import { Injectable, Logger } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    super({
+      log: [
+        { emit: "event", level: "query" },
+        { emit: "stdout", level: "info" },
+        { emit: "stdout", level: "warn" },
+        { emit: "stdout", level: "error" },
+      ],
+      errorFormat: process.env.NODE_ENV === "production" ? "minimal" : "pretty",
+    });
+  }
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+    } catch (error) {
+      Logger.error(error);
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
